07fb2dcc791bdf3f6c92ba8f0076d23eb3709f2e
Your Name <you@example.com>
2025-09-03 14:34:47 -0600

    Test Commands

---
+++ src/main/java/frc/robot/commands/ClawTestCommand.java
@@ -0,0 +1,28 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.constants.IntakeConstants;
+import frc.robot.subsystems.IntakeSubsystem;
+
+public class ClawTestCommand extends Command {
+    private final IntakeSubsystem intakeSubsystem;
+
+    public ClawTestCommand(IntakeSubsystem intakeSubsystem) {
+        this.intakeSubsystem = intakeSubsystem;
+    }
+
+    @Override
+    public void initialize() {
+        //Nothing
+    }
+
+    @Override
+    public void execute() {
+        intakeSubsystem.setClawVel(IntakeConstants.clawMaxAngVel);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        intakeSubsystem.setClawVel(0);
+    }
+}
