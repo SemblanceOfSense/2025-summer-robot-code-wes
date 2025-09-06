48370e823e07fc8373faa1e625758fc7eb724e71
50PercentSure <kevin.m.zheng@gmail.com>
2025-09-06 12:02:00 -0600

    Moved ShooterSubsystem into dev branch

---
+++ src/main/java/frc/robot/commands/ExampleShooterCommand.java
@@ -0,0 +1,25 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.ShooterSubsystem;
+
+//Example usage of Shooter Subsystem, not in Robot Container
+public class ExampleShooterCommand extends Command {
+    private final ShooterSubsystem shooterSubsystem;
+    private double shooterRPM;
+
+    public ExampleShooterCommand(ShooterSubsystem shooterSubsystem, double shooterRPM) {
+        this.shooterSubsystem = shooterSubsystem;
+        this.shooterRPM = shooterRPM;
+    }
+
+    @Override
+    public void execute() {
+        shooterSubsystem.setMotorRPM(shooterRPM);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        shooterSubsystem.setMotorRPM(0);
+    }
+}
---
+++ src/main/java/frc/robot/constants/ShooterConstants.java
@@ -0,0 +1,16 @@
+package frc.robot.constants;
+
+import frc.robot.utils.ConfigManager;
+
+public class ShooterConstants {
+    //TODO: Update constants and tune values
+    public static final int shooterMotorID = 0;
+
+    public static final double shooterFFKs = 0;
+    public static final double shooterFFKv = 0;
+    public static final double shooterFFKa = 0;
+
+    public static final double shooterP = 0;
+    public static final double shooterI = 0;
+    public static final double shooterD = 0;
+}
